9bf642b8e3d0f3087228e202d1bdefeb986935c8
bahman pournazari <bahmanpn@gmail.com>
2024-10-30 18:32:08 +0330

    added posts and post detail endpoints to project

--- blog_module/urls.py
+++ blog_module/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.urls import path
 from . import views
+
 
 app_name='blog_module'
 urlpatterns = [
     path('',views.indexApiView.as_view(),name='index'),
+    path('posts/', views.PostListCreate.as_view(), name='post_list_create'),
+    path('posts/<int:pk>/', views.PostRetrieveUpdateDestroy.as_view(), name='post_retrieve_update_destroy'),
 ]
--- blog_module/views.py
+++ blog_module/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
 
 class indexApiView(APIView):
     def get(self,request):
         return Response({'status':200,'msg':'success'})
+    
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
